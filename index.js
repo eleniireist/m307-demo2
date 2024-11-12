a182e214b4ec72672745cda33d3f20faeee847b9
eleni <eleni.reist@bbz-cfp.ch>
2024-11-12 19:02:26 +0000

    luege dases funktioniert mitem formular

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createApp } from "./config.js";
+import { createApp, upload } from "./config.js";
 
 const app = createApp({
   user: "blue_violet_3481",
@@ -18,12 +18,47 @@ app.get("/", async function (req, res) {
 
 /* Ordner */
 app.get("/ordner", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
+
   res.render("ordner", {});
 });
 
 /* Impressum */
 app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
+});
+
+/*new post */
+app.get("/new_post", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
+  res.render("new_post", {});
+});
+
+app.post("/create_post", upload.single("bild"), async function (req, res) {
+  await app.locals.pool.query(
+    "INSERT INTO posts (titel, text, bild, user_id) VALUES ($1, $2, $3, $4)",
+    [req.body.titel, req.body.text, req.file.filename, req.session.userid]
+  );
+  res.redirect("/");
+});
+
+/*Adresse für Formular registrieren*/
+app.get("/new_post", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
+
+  const posts = await app.locals.pool.query(
+    "select *, (select count(*) from likes where post_id = posts.id) as like_count, (select count(*) from comments where post_id = posts.id) as comment_count from posts"
+  );
+  res.render("start", { posts: posts.rows });
 });
 
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
